3876c295cf9a5350910f2b5f1876aedcc2064237
Steven Frasica <sfrasica1@gmail.com>
2019-12-30 05:25:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,4 +8,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  spielberg_movies = directors_database[0][:movies]
+  index = 0
+
+  while index < spielberg_movies.length do
+    titles = spielberg_movies[index][:title]
+    titles
+    index += 1
+  end
 end
